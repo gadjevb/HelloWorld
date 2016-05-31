c3d33281034b53e788a4d3db5af71fdb0295e88f
Borislav Gadjev <gadjevb@gmail.com>
2016-05-31 14:16:09 +0300

    The Hello world program

---
+++ src/com/clouway/hellogit/HelloGit.java
@@ -0,0 +1,8 @@
+package com.clouway.hellogit;
+
+public class HelloGit {
+    public static void main(String[] args){
+        System.out.print("Hello Git!!! \n");
+        System.out.print("Hello World!!! \n");
+    }
+}
