8c2dcebd7f3401df97f37cfc5e06cd3eb7858de4
Borislav Gadjev <gadjevb@gmail.com>
2016-06-01 15:53:21 +0300

    Some changes to my Greeting repository!

--- src/com/clouway/hellogit/HelloGit.java
+++ src/com/clouway/hellogit/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
         System.out.print("Hello Git! \n");
         System.out.print("Hello World! \n");
         System.out.print("its nice to meet you! \n");
-        System.out.print("My name is Borislav!");
+        System.out.print("My name is Borislav! \n");
+        System.out.print("Well, it's time for me to go, Bye!1! \n");
     }
 }
